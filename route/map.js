4a6de8ebba90351ef33e9d7e7293a808e4f81c43
Lorena Smienk <lorena.smienk@gmail.com>
2020-05-29 15:22:47 +0200

    style new icons added

--- route/map.js
+++ route/map.js
@@ -1,13 +1,12 @@
 import mapboxgl from 'mapbox-gl';
-import { getDurationString } from '../utils';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmdldHJpcCIsImEiOiJjamo3em4wdnUwdHVlM3Z0ZTNrZmd1MXoxIn0.aFteYnUc_GxwjTLGvB3uCg';
 
 const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/chargetrip/ck98fwwp159v71ip7xhs8bwts',
-  zoom: 6,
-  center: [8.3320104, 52.3758916],
+  zoom: 5.5,
+  center: [8.1320104, 52.3758916],
 });
 
 /**
@@ -98,8 +97,7 @@ const showLegs = legs => {
       points.push({
         type: 'Feature',
         properties: {
-          icon: 'pinlet',
-          description: index + 1,
+          icon: 'free-fast-pinlet',
         },
         geometry: leg.destination.geometry,
       });
@@ -108,7 +106,7 @@ const showLegs = legs => {
       points.push({
         type: 'Feature',
         properties: {
-          icon: 'destination',
+          icon: 'arrival',
         },
         geometry: leg.destination.geometry,
       });
@@ -122,22 +120,15 @@ const showLegs = legs => {
     layout: {
       'icon-image': '{icon}',
       'icon-allow-overlap': true,
-      'icon-size': 0.55,
+      'icon-size': 0.7,
       'icon-offset': [
         'case',
-        ['==', ['get', 'icon'], 'pinlet'],
-        ['literal', [50, -20]],
-        ['==', ['get', 'icon'], 'destination'],
-        ['literal', [0, -20]],
+        ['==', ['get', 'icon'], 'free-fast-pinlet'],
+        ['literal', [0, -15]],
+        ['==', ['get', 'icon'], 'arrival'],
+        ['literal', [0, -15]],
         ['literal', [0, 0]],
       ],
-      'text-field': ['get', 'description'],
-      'text-allow-overlap': true,
-      'text-offset': [-0.35, -1.6],
-      'text-size': 9,
-    },
-    paint: {
-      'text-color': '#FFFFFF',
     },
     source: {
       type: 'geojson',
@@ -146,49 +137,6 @@ const showLegs = legs => {
         features: points,
       },
     },
-  });
-
-  loadMarkers(legs);
-};
-
-/**
- * Draw single marker on a map.
- *
- * @param coordinates {array} point coordinates
- * @param label {string} point label
- * @param offset {array} marker offset
- * @param className {string} marker classname
- */
-const addMarker = (coordinates, label, offset, className = 'location-label') => {
-  const marker = document.createElement('div');
-  marker.innerHTML = label;
-  marker.className = className;
-  new mapboxgl.Marker(marker, { offset }).setLngLat(coordinates).addTo(map);
-};
-
-/**
- * Add markers to the map describing each point of the route.
- *
- * @param legs {array} route points
- */
-const loadMarkers = legs => {
-  // origin point marker (origin of the first leg)
-  const label = `From<br><strong>Amsterdam</strong>`;
-  const offset = [+30, -30];
-  addMarker(legs[0].origin.geometry.coordinates, label, offset);
-
-  legs.map((leg, index) => {
-    // charging stations
-    if (index !== legs.length - 1) {
-      const label = `<strong>${getDurationString(leg.chargeTime)}</strong> charge`;
-      const offset = [+35, -15];
-      addMarker(leg.destination.geometry.coordinates, label, offset, 'rounded-label');
-    } else {
-      // destination point marker (the last leg)
-      const label = `To<br><strong>Berlin</strong>`;
-      const offset = [-20, -45];
-      addMarker(leg.destination.geometry.coordinates, label, offset);
-    }
   });
 };
 
