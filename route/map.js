d8ceeeb9a14a61ca32dd180b47a1862d5a54429b
Lorena Smienk <50989058+lsmienk@users.noreply.github.com>
2020-06-12 12:46:50 +0200

    fix icon sizes

--- route/map.js
+++ route/map.js
@@ -137,8 +137,8 @@ const showLegs = legs => {
     layout: {
       'icon-image': '{icon}',
       'icon-allow-overlap': true,
-      'icon-size': ['case', ['==', ['get', 'icon'], 'free-fast-pinlet'], ['literal', 0.85], ['literal', 0.7]],
-      'icon-offset': [0, -15],
+      'icon-size': ['case', ['==', ['get', 'icon'], 'location-big'], ['literal', 0.7], ['literal', 0.8]],
+      'icon-offset': ['case', ['==', ['get', 'icon'], 'location_big'], ['literal', [0, 0]], ['literal', [0, -15]]],
     },
     source: {
       type: 'geojson',
@@ -159,18 +159,20 @@ const showLegs = legs => {
   });
 
   map.on('mouseenter', 'legs', e => {
-    map.getCanvas().style.cursor = 'pointer';
+    if (e.features[0].properties.icon !== 'arrival' && e.features[0].properties.icon !== 'location_big') {
+      map.getCanvas().style.cursor = 'pointer';
 
-    const coordinates = e.features[0].geometry.coordinates;
-    const description = e.features[0].properties.description;
+      const coordinates = e.features[0].geometry.coordinates;
+      const description = e.features[0].properties.description;
 
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+      popup
+        .setLngLat(coordinates)
+        .setHTML(description)
+        .addTo(map);
     }
-    popup
-      .setLngLat(coordinates)
-      .setHTML(description)
-      .addTo(map);
   });
 
   map.on('mouseleave', 'legs', function() {
