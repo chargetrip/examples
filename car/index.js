3caa28b32db0b390815f7765c9058c4f4b08b2a3
Lorena Smienk <50989058+lsmienk@users.noreply.github.com>
2020-06-24 15:57:03 +0200

    template added

--- car/index.js
+++ car/index.js
@@ -1,5 +1,6 @@
 import { createClient, defaultExchanges } from '@urql/core';
 import { getCarList } from './queries.js';
+import Mustache from 'mustache';
 
 /**
  * For the purpose of this example we use urgl - lightweights GraphQL client.
@@ -31,33 +32,7 @@ client
   .catch(error => console.log(error));
 
 const displayCarData = car => {
-  const tmpl = document.getElementById('car-profile');
-
-  document.body.appendChild(tmpl.content.cloneNode(true));
-  console.log(car);
-  const img = car.images[0].url;
-  //The make model and image of the car.
-  document.getElementById('make').innerHTML = car.make;
-  document.getElementById('model').innerHTML = car.carModel;
-  document.getElementById('car-image').src = img;
-
-  //Car details
-  document.getElementById('range').innerHTML = '---';
-  document.getElementById('battery').innerHTML = car.batteryUsableKwh + ' kWh';
-  document.getElementById('efficiency').innerHTML = car.batteryEfficiency.average + ' kWH / 100 km';
-  document.getElementById('plug').innerHTML = car.connectors[0].standard;
-
-  //Weather data
-  document.getElementById('city-mild').innerHTML = car.range.best.city + ' km';
-  document.getElementById('city-cold').innerHTML = car.range.worst.city + ' km';
-  document.getElementById('highway-mild').innerHTML = car.range.best.highway + ' km';
-  document.getElementById('highway-cold').innerHTML = car.range.worst.highway + ' km';
-  document.getElementById('combined-mild').innerHTML = car.range.best.combined + ' km';
-  document.getElementById('combined-cold').innerHTML = car.range.worst.combined + ' km';
-
-  //performance
-  document.getElementById('acceleration').innerHTML = car.acceleration + ' s';
-  document.getElementById('topspeed').innerHTML = car.topSpeed + ' km/h';
-  document.getElementById('power').innerHTML = car.power + ' KW';
-  document.getElementById('torque').innerHTML = car.torque + ' Nm';
+  let template = document.getElementById('template').innerHTML;
+  let rendered = Mustache.render(template, { make: car.make });
+  document.getElementById('target').innerHTML = rendered;
 };
