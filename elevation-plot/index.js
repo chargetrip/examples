8afbb6991830ff48b973a1e82d4866d023bde988
Khrystyna Skvarok <christina.skvarok@gmail.com>
2020-07-03 12:42:32 +0200

    fix: pair refacroting and claen up

--- elevation-plot/index.js
+++ elevation-plot/index.js
@@ -1,72 +1,7 @@
-import { createClient, createRequest, defaultExchanges, subscriptionExchange } from '@urql/core';
-import { pipe, subscribe } from 'wonka';
-import { SubscriptionClient } from 'subscriptions-transport-ws';
-import { createRoute, routeUpdate, getRoutePath } from './queries.js';
 import { drawRoute } from './map.js';
-import { loadGraph, imageLoader } from './graph';
 import * as mapboxPolyline from '@mapbox/polyline';
-import { displaySpecs, updateSpecs } from './journey-specs';
-
-/**
- * For the purpose of this example we use urgl - lightweights GraphQL client.
- * To establish a connection with Chargetrip GraphQL API you need to have an API key.
- * Read more about an authorisation in our documentation (https://docs.chargetrip.com/#authorisation).
- */
-const headers = {
-  'x-client-id': '5e8c22366f9c5f23ab0eff39',
-};
-
-const subscriptionClient = new SubscriptionClient('wss://api.chargetrip.io/graphql', {
-  reconnect: true,
-  connectionParams: headers,
-});
-
-const client = createClient({
-  url: 'https://api.chargetrip.io/graphql',
-  fetchOptions: {
-    method: 'POST',
-    headers,
-  },
-  exchanges: [
-    ...defaultExchanges,
-    subscriptionExchange({
-      forwardSubscription(operation) {
-        return subscriptionClient.request(operation);
-      },
-    }),
-  ],
-});
-
-/**
- * To create a route you need to:
- *
- * 1. create a new route and receive back its ID;
- * 2. subscribe to route updates in order to receive its details.
- */
-client
-  .mutation(createRoute)
-  .toPromise()
-  .then(response => {
-    const routeId = response.data.newRoute;
-
-    const { unsubscribe } = pipe(
-      client.executeSubscription(createRequest(routeUpdate, { id: routeId })),
-      subscribe(result => {
-        const { status, route } = result.data.routeUpdatedById;
-
-        // you can keep listening to the route changes to update route information
-        // for this example we want to only draw the initial route
-        if (status === 'done' && route) {
-          unsubscribe();
-          const routeData = result.data.routeUpdatedById.route;
-          drawRoutePolyline(routeData, response.data.newRoute);
-          loadGraph(routeData, routeData.elevationPlot);
-          imageLoader(routeData, routeData.legs);
-        }
-      }),
-    );
-  })
-  .catch(error => console.log(error));
+import { fetchRoute, fetchRoutePath } from './client';
+import { loadGraph, displayElevationData, displaySpecs } from './elevationGraph';
 
 /**
  * Draw a route on a map.
@@ -78,32 +13,22 @@ client
  * so we have to reverse every pair.
  *
  * @param data {object} route specification
+ * @param id {string} route ID
  */
+fetchRoute((routeId, routeData) => {
+  drawRoutePolyline(routeData, routeId);
+  loadGraph(routeData);
+});
+
 const drawRoutePolyline = (data, id) => {
   const decodedData = mapboxPolyline.decode(data.polyline);
   const reversed = decodedData.map(item => item.reverse());
+  const { elevationUp, elevationDown } = data;
 
-  client
-    .query(getRoutePath(id, reversed[0]))
-    .toPromise()
-    .then(response => {
-      displaySpecs(response.data);
-    })
-    .catch(error => console.log(error));
-  const map = drawRoute(reversed, data.legs);
-  updateSpecs(client, map, reversed, id);
-  displayRouteData(data);
-};
+  drawRoute(id, reversed, data.legs);
+  displayElevationData(elevationUp, elevationDown);
 
-/**
- * Show journey specific information like duration, consumption, costs etc.
- *
- * @param data {object} route specification
- */
-const displayRouteData = data => {
-  // The highest point in the elevation plot
-  document.getElementById('highest').innerHTML = `Up ${(data.elevationUp / 1000).toFixed(0)}km`;
-
-  // The lowest point in the elevation plot
-  document.getElementById('lowest').innerHTML = `Down ${(data.elevationDown / 1000).toFixed(0)}km`;
+  fetchRoutePath(id, reversed[0]).then(data => {
+    displaySpecs(data);
+  });
 };
--- elevation-plot/map.js
+++ elevation-plot/map.js
@@ -1,6 +1,6 @@
 import mapboxgl from 'mapbox-gl';
-import { drawClickedLine, drawPolyline, showLegs, addLineEnd } from './map-layers';
-import { findClosest } from './journey-specs';
+import { fetchRoutePath } from './client';
+import { positionElevationIndicator, displaySpecs } from './elevationGraph';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmdldHJpcCIsImEiOiJjamo3em4wdnUwdHVlM3Z0ZTNrZmd1MXoxIn0.aFteYnUc_GxwjTLGvB3uCg';
 
@@ -17,54 +17,242 @@ const map = new mapboxgl.Map({
  * @param coordinates {array} Array of coordinates
  * @param legs {array} route legs (stops) - each leg represents either a charging station, or via point or final point
  */
-export const drawRoute = (coordinates, legs) => {
+export const drawRoute = (id, coordinates, legs) => {
   if (map.loaded()) {
-    drawPolyline(map, coordinates);
-    showLegs(map, legs);
+    drawPolyline(coordinates);
+    showLegs(legs);
   } else {
     map.on('load', () => {
-      drawPolyline(map, coordinates);
-      showLegs(map, legs);
+      drawPolyline(coordinates);
+      showLegs(legs);
     });
   }
+
   map.on('mouseenter', 'polyline', () => {
     map.getCanvas().style.cursor = 'pointer';
   });
+
   map.on('mouseleave', 'polyline', () => {
     map.getCanvas().style.cursor = '';
   });
+
   map.on('click', 'polyline', e => {
-    const line = Object.assign([], coordinates);
     const location = [e.lngLat.lng, e.lngLat.lat];
-    let closest = findClosest(coordinates, location);
-    while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-      coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    }
-    splitPolyline(line, closest);
-    return;
+    const [closestPoint, closestPointIndex] = findClosestPoint(coordinates, location);
+    const position = closestPointIndex / coordinates.length;
+
+    fetchRoutePath(id, closestPoint).then(data => {
+      displaySpecs(data);
+    });
+
+    positionElevationIndicator(position);
+    splitPolyline([...coordinates], closestPoint);
   });
-  if (map.getLayer('chargers'));
-  else {
-    if (map.getLayer('end')) map.removeLayer('end');
-    if (map.getSource('point')) map.removeSource('point');
-    if (map.getLayer('clicked-polyline')) map.removeLayer('clicked-polyline');
-    if (map.getSource('clicked-source')) map.removeSource('clicked-source');
-    showLegs(map, legs);
-  }
-  return map;
 };
 
 /**
  * Create a second polyline up untill the point that was clicked.
  *
  * @param coordinates {array} the coordinates that need to be split
- * @param closest {array} the point at which the coordinates need to be split.
+ * @param closestPoint {array} the point at which the coordinates need to be split.
  */
-const splitPolyline = (coordinates, closest) => {
-  const end = coordinates[closest];
-  if (map.getLayer('chargers')) map.removeLayer('chargers');
-  if (map.getSource('chargers')) map.removeSource('chargers');
-  let clickedRoute = coordinates.splice(0, closest);
-  drawClickedLine(map, clickedRoute);
-  addLineEnd(map, end);
+const splitPolyline = (coordinates, closestPoint) => {
+  const clickedRoute = coordinates.splice(0, closestPoint);
+
+  drawClickedLine(clickedRoute);
+  addLineEnd(closestPoint);
+};
+
+/**
+ * Show the charging station, origin and destination on the map.
+ *
+ * Last leg of the route is a destination point.
+ * All other legs are either charging stations or via points (if the route has stops).
+ *
+ * @param legs {array} route legs (stops) - each leg represents either a charging station, or via point or final point
+ */
+export const showLegs = legs => {
+  if (!legs || legs.length === 0) return;
+  let route = [];
+
+  // we want to show the origin point on the map
+  // to do that we use the origin of the first leg
+  route.push({
+    type: 'Feature',
+    properties: {
+      icon: 'location_big',
+    },
+    geometry: legs[0].origin.geometry,
+  });
+
+  route.push({
+    type: 'Feature',
+    properties: {
+      icon: 'arrival',
+    },
+    geometry: legs[legs.length - 1].destination.geometry,
+  });
+
+  // draw origin and destination points on a map
+  if (!map.getLayer('route')) {
+    map.addLayer({
+      id: 'route',
+      type: 'symbol',
+      layout: {
+        'icon-image': '{icon}',
+        'icon-allow-overlap': true,
+        'icon-size': 0.9,
+        'icon-offset': ['case', ['==', ['get', 'icon'], 'arrival'], ['literal', [0, -15]], ['literal', [0, 0]]],
+      },
+      source: {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: route,
+        },
+      },
+    });
+  }
+};
+
+/**
+ * Draw route polyline on a map.
+ *
+ * @param coordinates {array} polyline coordinates
+ */
+export const drawPolyline = coordinates => {
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'LineString',
+          properties: {},
+          coordinates,
+        },
+      },
+    ],
+  };
+
+  map.addSource('polyline-source', {
+    type: 'geojson',
+    data: geojson,
+  });
+
+  map.addLayer({
+    id: 'polyline',
+    type: 'line',
+    options: 'beforeLayer',
+    source: 'polyline-source',
+    layout: {
+      'line-join': 'round',
+      'line-cap': 'round',
+    },
+    paint: {
+      'line-color': '#0078FF',
+      'line-width': 6,
+    },
+  });
+};
+
+/**
+ * With this function we will mark the route up until the point that was clicked.
+ * @param coordinates {array} The coordinates until the point that was clicked.
+ * @param map {object}
+ */
+export const drawClickedLine = coordinates => {
+  if (map.getLayer('clicked-polyline')) map.removeLayer('clicked-polyline');
+  if (map.getSource('clicked-source')) map.removeSource('clicked-source');
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'LineString',
+          properties: {},
+          coordinates,
+        },
+      },
+    ],
+  };
+
+  map.addSource('clicked-source', {
+    type: 'geojson',
+    data: geojson,
+  });
+
+  map.addLayer(
+    {
+      id: 'clicked-polyline',
+      type: 'line',
+      source: 'clicked-source',
+      layout: {
+        'line-join': 'round',
+        'line-cap': 'round',
+      },
+      paint: {
+        'line-color': '#EA8538',
+        'line-width': 7,
+      },
+    },
+    'route',
+  );
+};
+
+/**
+ * Display the end of the clicked polyline.
+ * @param map {object}
+ * @param end {array} The coordinates of the end of the clicked line.
+ */
+export const addLineEnd = end => {
+  if (map.getLayer('end')) map.removeLayer('end');
+  if (map.getSource('point')) map.removeSource('point');
+  map.addSource('point', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: [
+        {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: end,
+          },
+        },
+      ],
+    },
+  });
+  map.addLayer({
+    id: 'end',
+    type: 'symbol',
+    source: 'point',
+    layout: {
+      'icon-image': 'elipse',
+      'icon-size': 1.2,
+    },
+  });
+};
+
+/**
+ * Find the closest point in the polyline.
+ *
+ * @param polyline {array} polyline coordinates.
+ * @param location {array} the location that was clicked on the polyline.
+ */
+const findClosestPoint = (polyline, location) => {
+  const [x1, y1] = location;
+  let closestPoint = Math.sqrt(Math.pow(Math.abs(x1 - polyline[0][0]), 2) + Math.pow(Math.abs(y1 - polyline[0][1]), 2));
+  let closestPointIndex = 0;
+
+  for (let i = 1; i < polyline.length - 1; i++) {
+    let distance = Math.sqrt(Math.pow(Math.abs(x1 - polyline[i][0]), 2) + Math.pow(Math.abs(y1 - polyline[i][1]), 2));
+    if (distance <= closestPoint) {
+      closestPoint = distance;
+      closestPointIndex = i;
+    }
+  }
+
+  return [closestPoint, closestPointIndex];
 };
