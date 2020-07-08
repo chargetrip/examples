3c4e364be04508166519932955b1c6da5d638be4
Lorena Smienk <50989058+lsmienk@users.noreply.github.com>
2020-07-08 12:27:44 +0200

    variable changed

--- stations/map.js
+++ stations/map.js
@@ -23,7 +23,7 @@ const selectPinlet = station => `${station.status}-${station.speed}`;
  */
 
 export const loadStation = stations => {
-  document.getElementById('stationAmount').innerHTML = 'Showing ' + stations.length + ' stations';
+  document.getElementById('stationAmount').innerHTML = stations.length;
   if (map.getLayer('path')) map.removeLayer('path');
   if (map.getSource('path')) map.removeSource('path');
 
