4d8ca316d45f760557a1018c63fd7b2beca3ab67
Lorena Smienk <50989058+lsmienk@users.noreply.github.com>
2020-07-31 13:59:54 +0200

    rainbowroad

--- state-of-charge/map.js
+++ state-of-charge/map.js
@@ -83,6 +83,7 @@ const drawPolyline = coordinates => {
 
   map.addSource('polyline-source', {
     type: 'geojson',
+    lineMetrics: true,
     data: geojson,
   });
 
@@ -96,8 +97,27 @@ const drawPolyline = coordinates => {
       'line-cap': 'round',
     },
     paint: {
-      'line-color': '#0078FF',
-      'line-width': 3,
+      'line-color': 'red',
+      'line-width': 6,
+      // 'line-gradient' must be specified using an expression
+      // with the special 'line-progress' property
+      'line-gradient': [
+        'interpolate',
+        ['linear'],
+        ['line-progress'],
+        0,
+        'blue',
+        0.1,
+        'royalblue',
+        0.3,
+        'cyan',
+        0.5,
+        'lime',
+        0.7,
+        'yellow',
+        1,
+        'red',
+      ],
     },
   });
 };
