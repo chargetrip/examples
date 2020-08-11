cee19e7d453b011b4b5278d16678e9b24c8d3d1d
Khrystyna Skvarok <christina.skvarok@gmail.com>
2020-08-11 09:24:23 +0200

    fix: show zero stations if responce is empty

--- state-of-charge/map.js
+++ state-of-charge/map.js
@@ -1,5 +1,4 @@
 import mapboxgl from 'mapbox-gl';
-import { sliderUpdate } from './slider';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY2hhcmdldHJpcCIsImEiOiJjazhpaG8ydTIwNWNpM21ud29xeXc2amhlIn0.rGKgR3JfG9Z5dCWjUI_oGA';
 
--- state-of-charge/slider.js
+++ state-of-charge/slider.js
@@ -1,28 +1,25 @@
 import { fetchRoute } from './client';
 import { drawRoutePolyline } from './index';
-import { getDurationString } from '../utils';
 
-const initialSOC = 435;
 const rangeSlider = document.getElementById('range');
-const rangeThumb = document.getElementById('range-thumb');
-rangeSlider.value = initialSOC;
 
-export const updateRangeSliderValue = () => {
-  const percent = rangeSlider.value / 435;
-  const newPosition = percent * rangeSlider.offsetWidth - 70 * percent;
+rangeSlider.addEventListener('input', () => {
+  const percent = rangeSlider.value / rangeSlider.max;
+  const thumbPosition = percent * rangeSlider.offsetWidth - 70 * percent;
 
+  const rangeThumb = document.getElementById('range-thumb');
   rangeThumb.innerHTML = `${rangeSlider.value} km`;
-  rangeThumb.style.left = `calc((${newPosition}px))`;
-};
+  rangeThumb.style.left = `calc((${thumbPosition}px))`;
+});
+
+rangeSlider.addEventListener('change', () => {
+  document.getElementById('reroutingNotification').style.display = 'flex';
+
+  rangeSlider.value,
+    routeData => {
+      drawRoutePolyline(routeData);
+      document.getElementById('reroutingNotification').style.display = 'none';
+    };
+});
 
 export const getStateOfCharge = () => rangeSlider.value;
-rangeSlider.addEventListener('input', () => {
-  updateRangeSliderValue();
-});
-rangeSlider.addEventListener('change', () => {
-  document.getElementById('calculating').style.display = 'flex';
-  fetchRoute(rangeSlider.value, routeData => {
-    document.getElementById('duration').innerHTML = `${getDurationString(routeData.duration ?? 0)}`;
-    drawRoutePolyline(routeData);
-  });
-});
