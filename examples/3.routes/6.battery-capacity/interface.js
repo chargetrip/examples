d35f53e1c27e776b27daad7ac4d92b5f6e71be13
Wouter <wouter@thisisnota.studio>
2023-07-05 09:58:41 +0200

    fix: some additional meta tag work

--- examples/3.routes/6.battery-capacity/interface.js
+++ examples/3.routes/6.battery-capacity/interface.js
@@ -5,7 +5,6 @@ import { getDurationString } from '../../../utils';
 // This example uses a Tesla 75D (2016-2019). By default it has a useable battery capacity of 72.5 kWh and
 // a Chargetrip best range of 389. These two values are interchangeable when adjusting the capacity.
 // In a real application you would fetch these values on a per car basis using a car request.
-// See our car example on how you would get these details; https://examples.chargetrip.com/?id=car-list
 const usableBatteryCapacityInKWH = 72.5;
 const chargetripBestRange = 389;
 
