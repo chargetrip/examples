0f46daba40f148767d8a18386ea1eb90ad0c33f1
laurensvdmaas <laurensvdmaas@gmail.com>
2024-06-03 09:24:35 +0200

    fix: remove empty example

--- examples/4.tile-service/2.tile-json/helper.js
+++ examples/4.tile-service/2.tile-json/helper.js
@@ -8,14 +8,15 @@
  */
 function latLngToPixel(lat, lng, zoom) {
   // Convert latitude to radians.
-  const latitudeInRadians = lat * Math.PI / 180;
+  const latitudeInRadians = (lat * Math.PI) / 180;
 
   // Calculate the width of the map in tiles at the given zoom level.
   const mapWidthInTiles = Math.pow(2, zoom);
 
   // Convert latitude and longitude to pixel coordinates.
   const x = mapWidthInTiles * ((lng + 180) / 360);
-  const y = mapWidthInTiles * (1 - (Math.log(Math.tan(latitudeInRadians) + (1 / Math.cos(latitudeInRadians))) / Math.PI)) / 2;
+  const y =
+    (mapWidthInTiles * (1 - Math.log(Math.tan(latitudeInRadians) + 1 / Math.cos(latitudeInRadians)) / Math.PI)) / 2;
 
   return [x, y];
 }
